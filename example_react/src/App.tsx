0735595622cbf75deed8afa2b9f4adc6eb86f26c
Nuno Ribeiro <nmpribeiro@gmail.com>
2023-03-09 12:00:36 +0100

    adds examples

---
+++ example_react/src/App.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+// import logo from "./logo.svg";
+import SayHello from "vuera_ts_lib";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SayHello name="Prateek" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
