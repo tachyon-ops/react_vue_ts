dcf9e78fb9e9c95e3d4282f4970e9c12a287667d
Nuno Ribeiro <nmpribeiro@gmail.com>
2023-03-21 11:05:09 +0100

    working vue2 example

---
+++ example_vue2/src/app/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <p>Hello World!</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'app',
+  components: {},
+};
+</script>
---
+++ example_vue2/src/main.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import App from './app/App.vue';
+
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App },
+});
